4b9c89e1a2578988420a91717a64130400104826
Mariam <mariamtct0@gmail.com>
2024-10-13 16:41:39 +0300

    Added a Category class for database connection

---
+++ BookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWeb.Models
+{
+    public class Category
+    {
+        //primary key of the table
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
